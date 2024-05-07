e17e99e2c84bab598c98c4f4b3f591aa0dcdf80b
Ramesh63822 <enginerameshprofessional@gmail.com>
2024-05-07 14:10:00 +0530

    created carousal and movie cards but not in alignment

--- src/components/Carousal/Carousal.jsx
+++ src/components/Carousal/Carousal.jsx
@@ -1,13 +1,46 @@
-import { FaBeer } from "react-icons/fa";
+import React, { useState } from 'react'
+import ranam from './asserts Carousal/ranam-carousal.jpg'
+import jawan from './asserts Carousal/jawan-carousal1.jpg'
+import siren from './asserts Carousal/siren-carousal1.jpg'
+import lalsalam from './asserts Carousal/lalsalam-carousal.jpg'
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 
-export default function Carousal({slides}){
-    return (
-    <div className="overflow-hidden relative">
-    <div className="flex"> 
-        {slides.map((s)=>{
-            return <img src ={s} />
-        })}
+
+
+const Carousal = () => {
+
+    const slides=[ranam,jawan,siren,lalsalam]
+    const [index,setIndex] = useState(0)
+
+    const beforeSlide = ()=>{
+        const firstSlide = index === 0
+        const newIndex = firstSlide? slides.length -1 : index -1
+        setIndex(newIndex)
+    }
+
+    const afterSlide = ()=>{
+        const lastSlide = index === slides.length - 1
+        const newIndex= lastSlide ? 0 : index + 1
+        setIndex(newIndex)
+        
+    }
+
+
+  return (
+    <div className=' h-[500px] w-[1345px] my-5 px-3 relative group'>
+        <div style={{backgroundImage: `url(${slides[index]})` }} className='w-full h-[400px] rounded-2xl bg-center bg-cover duration-500'>
+        </div>
+        <div className='hidden group-hover:block absolute top-[40%] translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer '>
+            <BsChevronCompactLeft onClick={beforeSlide} size={30}/>
+        </div>
+        <div className='hidden group-hover:block absolute top-[40%] translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+            <BsChevronCompactRight onClick={afterSlide} size={30}/>
+        </div>
+        
+
+
     </div>
-    </div>
-    );
+  )
 }
+
+export default Carousal
