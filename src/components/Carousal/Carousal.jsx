909623f8799cb38bd1d32441befca86c7dcfb170
Ramesh63822 <enginerameshprofessional@gmail.com>
2024-05-08 09:48:29 +0530

    created final outline of carousal and sample card component

--- src/components/Carousal/Carousal.jsx
+++ src/components/Carousal/Carousal.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
-import ranam from './asserts Carousal/ranam-carousal.jpg'
-import jawan from './asserts Carousal/jawan-carousal1.jpg'
-import siren from './asserts Carousal/siren-carousal1.jpg'
-import lalsalam from './asserts Carousal/lalsalam-carousal.jpg'
+import kalakuthu from './asserts Carousal/carousal1.jpg'
+import superDeluxe from './asserts Carousal/carousal2.jpg'
+import kakkiSattai from './asserts Carousal/carousal3.jpg'
+import pandiyaNadu from './asserts Carousal/carousal4.jpg'
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 
 
@@ -10,7 +10,7 @@ import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 
 const Carousal = () => {
 
-    const slides=[ranam,jawan,siren,lalsalam]
+    const slides=[kalakuthu,superDeluxe,kakkiSattai,pandiyaNadu]
     const [index,setIndex] = useState(0)
 
     const beforeSlide = ()=>{
@@ -28,13 +28,15 @@ const Carousal = () => {
 
 
   return (
-    <div className=' h-full w-full  relative group'>
-        <div style={{backgroundImage: `url(${slides[index]})` }} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'>
+    <div style={{width:'1320px', height:'450px', margin:'15px'}} className='  relative group'>
+        
+        <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'>
+            <img style={{width:'1320px', height:'450px', borderRadius:'15px'}} src={slides[index]} alt="" />
         </div>
-        <div className='hidden group-hover:block absolute top-[40%] translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer '>
+        <div className='hidden group-hover:block absolute top-[50%] translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer '>
             <BsChevronCompactLeft onClick={beforeSlide} size={30}/>
         </div>
-        <div className='hidden group-hover:block absolute top-[40%] translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <div className='hidden group-hover:block absolute top-[50%] translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
             <BsChevronCompactRight onClick={afterSlide} size={30}/>
         </div>
         
--- src/components/Demo/Demo.jsx
+++ src/components/Demo/Demo.jsx
@@ -4,51 +4,12 @@ import React from 'react'
 
 const Demo = (props) => {
   return (
-    <div className='flex items-center justify-center min-h-screen container mx-auto'>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-
-            {/* Card1 */}
-            <div className='rounded-xl shadow-lg'> 
-                <div className='p-5 flex flex-col'>
-                    <div className=' rounded-xl overflow-hidden '>
-                        <img className='h-64 w-'  src={props.image} alt="" />
-                    </div>
-                    <h5 className='text-2xl md:text-3xl font-medium mt-3'>hello</h5>
-                    <p className='text-slate-500 text-lg mt-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                    <a href="#" className='text-center bg-blue-400 text-blue-700 py-2 rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out'>Explore</a>
-                </div>
-            </div>
-
-            {/* Card1
-            <div className='rounded-xl shadow-lg'> 
-                <div className='p-5 flex flex-col'>
-                    <div className='rounded-xl overflow-hidden'>
-                        <img src={animal} alt="" />
-                    </div>
-                    <h5 className='text-2xl md:text-3xl font-medium mt-3'>hello</h5>
-                    <p className='text-slate-500 text-lg mt-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                    <a href="#" className='text-center bg-blue-400 text-blue-700 py-2 rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out'>Explore</a>
-                </div>
-            </div> */}
-
-            {/* Card1
-            <div className='rounded-xl shadow-lg'> 
-                <div className='p-5 flex flex-col'>
-                    <div className='rounded-xl overflow-hidden'>
-                        <img src={animal} alt="" />
-                    </div>
-                    <h5 className='text-2xl md:text-3xl font-medium mt-3'>hello</h5>
-                    <p className='text-slate-500 text-lg mt-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                    <a href="#" className='text-center bg-blue-400 text-blue-700 py-2 rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out'>Explore</a>
-                </div>
-            </div> */}
-
-            
-
-            
-
-
-        </div>
+    <div style={{width:'300px' , height:'500px', marginTop:'15px', marginLeft:'15px', marginBottom:'15px', backgroundColor:'#5ae6ab'}}>
+      <img src={props.image} alt='' style={{width:'300px', height:'400px'}} />
+      <div style={{padding:'5px'}}>
+        <div className="font-bold text-xl mb-2">hello</div>
+        <p className="text-gray-700 text-base">nothing</p>
+      </div>
     </div>
   )
 }
